f93a4a8ffec426de710d437d49df21ffe3393f54
AYUMI-WORK-PC\ayumi <ayumi.suzuki@dictux.biz>
2024-09-05 14:31:21 +0900

    20240827 テキストFB修正

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,34 @@ jQuery(function ($) {
       return false;
     });
   });
+
+  //要素の取得とスピードの設定
+  var box = $(".js-fadeImage"),
+    speed = 700;
+
+  //.colorboxの付いた全ての要素に対して下記の処理を行う
+  box.each(function () {
+    $(this).append('<div class="color"></div>');
+    var color = $(this).find($(".color")),
+      image = $(this).find("img");
+    var counter = 0;
+
+    image.css("opacity", "0");
+    color.css("width", "0%");
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on("inview", function () {
+      if (counter == 0) {
+        $(this)
+          .delay(200)
+          .animate({ width: "100%" }, speed, function () {
+            image.css("opacity", "1");
+            $(this).css({ left: "0", right: "auto" });
+            $(this).animate({ width: "0%" }, speed);
+          });
+        counter = 1;
+      }
+    });
+  });
 });
 // メインビジュアルスライダー
 const mvSwiper = new Swiper("#swiper01", {
@@ -30,32 +58,4 @@ const campaignSwiper = new Swiper("#swiper02", {
       spaceBetween: 40,
     },
   },
-});
-
-//要素の取得とスピードの設定
-var box = $(".js-fadeImage"),
-  speed = 700;
-
-//.colorboxの付いた全ての要素に対して下記の処理を行う
-box.each(function () {
-  $(this).append('<div class="color"></div>');
-  var color = $(this).find($(".color")),
-    image = $(this).find("img");
-  var counter = 0;
-
-  image.css("opacity", "0");
-  color.css("width", "0%");
-  //inviewを使って背景色が画面に現れたら処理をする
-  color.on("inview", function () {
-    if (counter == 0) {
-      $(this)
-        .delay(200)
-        .animate({ width: "100%" }, speed, function () {
-          image.css("opacity", "1");
-          $(this).css({ left: "0", right: "auto" });
-          $(this).animate({ width: "0%" }, speed);
-        });
-      counter = 1;
-    }
-  });
 });
